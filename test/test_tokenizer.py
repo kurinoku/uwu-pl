ea494a703fcd95a4f5e9087638ab417965b35715
kurinoku <31811692+kurinoku@users.noreply.github.com>
2023-03-20 22:26:29 -0300

    add decimals in test_tokenizer

--- test/test_tokenizer.py
+++ test/test_tokenizer.py
@@ -12,12 +12,14 @@ O.O @_@
 UwU @_@
 :v @_@ 2
 UwU @_@
+:v @_@ 2.5
+UwU @_@
 """
 
 tkn = Tokenizer.from_string(script)
 #for t in tkn:
 #    print(t)
-for i in range(15):
+for i in range(50):
     print(tkn.next())
 
 #for i, t in enumerate(tkn):
